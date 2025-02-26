6d87e0f35cda40693d02aac5d7b0cdb3d252cc76
Oduyale Enoch <oduyaleenoch@gmail.com>
2025-02-26 18:21:38 +0100

    fixed home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {Home} from './pages/home'
+import {Home} from './pages/Home'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { Login } from './pages/Login';
 import { Register } from './pages/Register';
